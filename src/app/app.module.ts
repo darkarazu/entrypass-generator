18d1aa935151d17bd8be8b878357f8ec789cda9e
darkarazu <jalexander.rvera@gmail.com>
2022-10-06 15:40:55 +0100

    Updated 2ndary packages to latest versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -21,10 +20,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     MatDatepickerModule,
-    ReactiveFormsModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
